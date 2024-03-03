3a5c94c3e6985052ff572a62573c45df9587a0b4
Akash-Nipunge <nipungeakash9359@gmail.com>
2024-03-03 22:35:07 +0530

    update

--- frontend/src/home/NavHome.jsx
+++ frontend/src/home/NavHome.jsx
@@ -34,19 +34,16 @@ const NavHome = () => {
         <FontAwesomeIcon icon={faTimes} size="lg" />
       </button>
       <ul className='flex flex-col items-start p-4'>
-<<<<<<< HEAD
         <li className='text-gray-800 py-2'><a href='#'>Home</a></li>
         <li className='text-gray-800 py-2'><a href='#'>About</a></li>
         <li className='text-gray-800 py-2'><a href='#'>Services</a></li>
         <li className='text-gray-800 py-2'><a href='#'>Contact</a></li>
         <button className="w-full bg-green-500 p-2 rounded-md"><a href='http://localhost:5173/user/login'>Login</a></button>
-=======
         <li className='text-gray-800 py-2'><a href=''>Home</a></li>
         <li className='text-gray-800 py-2'><a href='#events'>Events</a></li>
         <li className='text-gray-800 py-2'><a href='#work'>Work</a></li>
         <li className='text-gray-800 py-2'><a href='#feedback'>Feedback</a></li>
         <button className="w-full bg-green-500 p-2 rounded-md"><a href='https://educative-beige.vercel.app/user/login'>Login</a></button>
->>>>>>> 16ffe5e8aa967caa6e7842430b30fe05b813f60e
       </ul>
     </div>
   </div>
